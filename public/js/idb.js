60b6d6c895591c1cd6349baecf11fd1902c943f7
Jacekhacking <82614083+Jacekhacking@users.noreply.github.com>
2021-09-06 18:20:04 -0600

    Deploying

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,6 +1,7 @@
 let db;
 
-const request = indexedDB.open('budget_tracker', 1);
+// const request = indexedDB.open('budget_tracker', 1);
+const request = indexedDB.open('pwa-budget-tracker-jh');
 
 request.onupgradeneeded = function (event){
     const db = event.target.result;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
@@ -16,7 +17,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(MONGODB_URI, {
   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
